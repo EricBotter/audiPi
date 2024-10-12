040bf8729aa2ea309ac3b0b85f6e1cd640f67b38
Eric Botter <eric.botter@gmail.com>
2024-10-12 16:56:08 +0200

    App can open CD tray
    also removed Qt deps

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
-#include <QApplication>
-#include <QPushButton>
+#include <iostream>
+
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/cdrom.h>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+
+    int cdrom_fd = open("/dev/cdrom", O_RDONLY | O_NONBLOCK);
+
+    if (cdrom_fd < 0) {
+        std::cerr << "cannot open /dev/cdrom" << std::endl;
+        return -1;
+    }
+
+    int eject_error = ioctl(cdrom_fd, CDROMEJECT, 0);
+
+    if (eject_error == ENOSYS) {
+        std::cerr << "/dev/cdrom cannot eject" << std::endl;
+        return -1;
+    }
+
+    if (eject_error == EBUSY) {
+        std::cerr << "/dev/cdrom in use or similar" << std::endl;
+        return -1;
+    }
+
+    return 0;
 }
